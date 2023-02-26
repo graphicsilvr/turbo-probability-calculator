d834d811dde68f5d2796beee1de2f195baec9709
Replit user <>
2023-02-26 19:04:00 +0100

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # Probability Calculator
+This is a Python program for estimating the probability of drawing certain balls randomly from a hat.
 
-This is the boilerplate for the Probability Calculator project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/probability-calculator
+# Getting Started
+To use this program, you will need to have Python 3 installed on your computer.
+
+Clone this repository to your local machine.
+Open a terminal and navigate to the project directory.
+Run the program using the command python main.py.
+How to Use
+The program consists of two files: prob_calculator.py and main.py.
+
+prob_calculator.py contains the Hat class and the experiment function. The Hat class takes a variable number of arguments that specify the number of balls of each color that are in the hat, and has a draw method that removes balls at random from the hat and returns them as a list. The experiment function takes a Hat object, an expected_balls dictionary, the number of balls to draw in each experiment, and the number of experiments to perform, and returns the estimated probability of drawing the expected balls.
+
+main.py is an example of how to use the Hat class and experiment function. It creates a Hat object with a specified number of balls of different colors, and runs an experiment to estimate the probability of drawing a certain number of balls of each color.
+
+# Example
+Here is an example of how to use the program:
+
+python
+from prob_calculator import Hat, experiment
+
+# Create a hat object
+hat = Hat(red=5, blue=3, green=2)
+
+# Set the expected balls
+expected_balls = {'red': 2, 'blue': 1}
+
+# Set the number of balls to draw and experiments to perform
+num_balls_drawn = 4
+num_experiments = 1000
+
+# Run the experiment and print the result
+probability = experiment(hat=hat, expected_balls=expected_balls, num_balls_drawn=num_balls_drawn, num_experiments=num_experiments)
+print(f"Probability of drawing {expected_balls} in {num_balls_drawn} balls: {probability}")
+This will output the estimated probability of drawing 2 red balls and 1 blue ball in 4 balls drawn from the hat, based on 1000 experiments. The probability will be different each time the code is run due to the random nature of the experiments.
+
+# Authors
+This program was created by Sylvious.
+
+# Lisence
+This project is licensed under the MIT License or the Apache License. Please see the LICENSE file for details.
--- prob_calculator.py
+++ prob_calculator.py
@@ -1,8 +1,77 @@
 import copy
 import random
-# Consider using the modules imported above.
+
+import copy
+import random
 
 class Hat:
+    def __init__(self, **balls):
+        """
+        Initialize a new hat with contents specified by the arguments.
+        Args:
+            **balls: keyword arguments indicating the balls and their quantity in the hat
+        """
+        self.contents = []
+        for ball, count in balls.items():
+            for i in range(count):
+                self.contents.append(ball)
 
+    def draw(self, num):
+        """
+        Remove `num` random balls from the hat's contents and return them.
+        If `num` is greater than the number of balls in the hat, return all the balls.
+        Args:
+            num: the number of balls to draw
+        Returns:
+            A list of strings representing the balls drawn from the hat
+        """
+        if num >= len(self.contents):
+            return self.contents.copy()
+        balls_drawn = []
+        for i in range(num):
+            ball_index = random.randrange(len(self.contents))
+            balls_drawn.append(self.contents.pop(ball_index))
+        return balls_drawn
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    """
+    Perform a number of experiments drawing `num_balls_drawn` balls from the `hat`.
+    In each experiment, count the number of times that at least the number of balls specified in
+    `expected_balls` was drawn. Return the probability of that event as the fraction of experiments
+    in which it occurred.
+    Args:
+        hat: a Hat object representing the initial contents of the hat
+        expected_balls: a dictionary specifying the minimum number of times to draw each color
+        num_balls_drawn: the number of balls to draw in each experiment
+        num_experiments: the number of experiments to perform
+    Returns:
+        The probability of drawing at least the number of expected balls in `num_experiments`
+    """
+    # Convert `expected_balls` to a list of ball strings, duplicating each ball as many times as required
+    expected_balls_list = []
+    for ball, count in expected_balls.items():
+        for i in range(count):
+            expected_balls_list.append(ball)
+    # Count the number of successful experiments
+    num_successful_experiments = 0
+    for i in range(num_experiments):
+        # Create a deep copy of the hat so we can modify it without changing the original contents
+        hat_copy = copy.deepcopy(hat)
+        # Draw the required number of balls from the hat
+        balls_drawn = hat_copy.draw(num_balls_drawn)
+        # Make a copy of the expected balls list so we can modify it without changing the original
+        expected_balls_copy = expected_balls_list.copy()
+        success = True
+        # For each ball in the expected list, remove it from the drawn balls if possible.
+        # If not possible, the experiment is unsuccessful and we break out of the loop
+        for ball in expected_balls_copy:
+            if ball in balls_drawn:
+                balls_drawn.remove(ball)
+            else:
+                success = False
+                break
+        if success:
+            num_successful_experiments += 1
+    # Calculate the probability of drawing at least the expected number of balls
+    return num_successful_experiments / num_experiments
+
